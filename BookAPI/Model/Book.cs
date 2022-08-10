30e02bf5902f485abeb23a12c01a15c0cfc47cd4
Michel BNasc <86130511+MichelBNasc@users.noreply.github.com>
2022-08-10 08:03:57 -0300

    Add files via upload

---
+++ BookAPI/Model/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BookAPI.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+    }
+}
